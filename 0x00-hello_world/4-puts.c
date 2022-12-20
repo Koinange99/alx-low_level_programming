3833b872fbf25967249f69e1d1eb1d59537c8e83
koinange99 <zackmackoy@.com>
2022-12-20 08:20:32 +0300

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - Entry point
+ *
+ * Description:'the programs description'
+ * Return: Always 0 (Success)
  */
-int main(void);
+int main(void)
 {
-          puts("\"Programming is like building a multilingual puzzle");
-	  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
